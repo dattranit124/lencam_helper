341f312ba0840b94d13839e0e9746a7cabcb4c12
dattranit124 <tranhocdatpc@gmail.com>
2022-01-14 10:03:52 +0700

    update

--- lencam_helper/components/modules/header.vue
+++ lencam_helper/components/modules/header.vue
@@ -1,20 +1,57 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<!-- left -->
-		<div>
-				
-		</div>
-		<!-- right -->
-		<div>
-
-		</div>
-	</nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid my-3 mx-5">
+      <a class="navbar-brand" href="#">
+        <img src="assets/logo/logo.png" alt="" height="30" />
+        <span class="fst-italic fw-light">trung tâm trợ giúp</span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+	  <div class="collapse navbar-collapse fs-5" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 menu-item">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Trang chủ</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link active" href="#">Nhà phát triển
+		  </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Hỗ trợ
+			<i class="fas fa-chevron-down"></i>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+    </div>
+  </nav>
 </template>
 <script>
-export default {
-	
-}
+export default {};
 </script>
 <style>
-	
+.menu-item>li  {
+	margin: 0 1rem !important;
+	white-space: nowrap;
+}
 </style>
--- lencam_helper/nuxt.config.js
+++ lencam_helper/nuxt.config.js
@@ -1,58 +1,52 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lencam_helper',
+    title: "lencam_helper",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet',  href: '/assets/css/bootstrap.min.css' },
-      { rel: 'stylesheet',  href: '/assets/css/styles.css' },
-
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "/assets/css/bootstrap.min.css" },
+      { rel: "stylesheet", href: "/assets/css/style.css" },
+      {
+        rel: "stylesheet",
+        href: "/assets/fontawesome-free-5.15.4-web/css/all.css",
+      },
     ],
     script: [
-      { src:'/assets/js/bootstrap.bundle.min.js' },
-      { src:'/assets/js/bootstrap.main.js' },
-    ]
-
+      { src: "/assets/js/bootstrap.bundle.min.js" },
+      { src: "/assets/js/bootstrap.main.js" },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    "@nuxtjs/axios",
-    "@/modules/integrations"
-  ],
+  modules: ["@nuxtjs/axios", "@/modules/integrations"],
   axios: {
-    baseURL: process.env.API_URL || "https://api.lencam.com"
-},
+    baseURL: process.env.API_URL || "https://api.lencam.com",
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
   server: {
-      port: '8080',
-    },
-}
+    port: "8080",
+  },
+};
