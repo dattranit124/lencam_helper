d3e3e3fdd39b97527cbf505bef26be926b08c412
dattranit124 <tranhocdatpc@gmail.com>
2022-01-22 10:21:24 +0700

    Update copy-right.vue

--- lencam_helper/components/modules/copy-right.vue
+++ lencam_helper/components/modules/copy-right.vue
@@ -1,6 +1,6 @@
 <template >
 	<div class="bg-light bg-gradient">
-		<p class="text-center my-3 text-secondary fs-6">© 2022 ShopBase Help Center</p>
+		<p class="text-center my-3 text-secondary fs-6">© 2022 LenCam Help Center</p>
 	</div>
 </template>
 <script>
